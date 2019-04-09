eb67aad182820c950a2e0cb45f34e16d1f998cde
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-04-09 16:46:07 +0200

    Updating travis and coverage status links

--- api/models/user.js
+++ api/models/user.js
@@ -7,7 +7,6 @@ const users = [
     phone: '+250783067644',
     userName: 'niyoceles',
     password: 'celes123',
-    token: 'a41f8a8dbb67735da4d0f1ac100975ea3dc1409b022d4043d8584f0a18c3efbe',
   },
 ];
 
