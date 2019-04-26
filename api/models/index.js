612c69bfb4d7b8459c57799103e2fb1d7ba39cb9
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-04-26 10:06:33 +0200

    Updating and install  Database to Heroku

--- api/models/index.js
+++ api/models/index.js
@@ -2,20 +2,10 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const env = (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev') ? `_${process.env.NODE_ENV}` : '';
 
-const config = {
-  host: process.env[`DB_HOST${env}`],
-  user: process.env[`DB_USER${env}`],
-  database: process.env[`DB_NAME${env}`],
-  password: process.env[`DB_PASSWORD${env}`],
-  port: process.env[`DB_PORT${env}`],
-};
-
-const pool = new pg.Pool(config);
-
-pool.on('connect', () => {
-  console.log('connected to the Database');
+const connString = process.env.DATABASE_URL;
+const pool = new pg.Pool({
+  connectionString: connString,
 });
 
 export default {
