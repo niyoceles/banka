555fd680a5f4cfd65533f5bd7308a116c08eb80a
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-05-27 11:43:10 +0200

    [Bug #166253839 Updating maintainability code again

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -7,13 +7,16 @@ import db from '../models';
 dotenv.config();
 
 class UsersController {
-
-  static async getAllUserAccounts(req, res) {
+  static checkUser(req, res) {
     if (req.decodedToken.type === 'client') {
       return res.status(401).json({
         message: 'Not allowed to access this feature, staff Only',
       });
     }
+  }
+
+  static async getAllUserAccounts(req, res) {
+    UsersController.checkUser(req, res);
     try {
       let checkUserAccounts = '';
       checkUserAccounts = await db.query('SELECT * FROM users');
@@ -29,12 +32,11 @@ class UsersController {
           data: checkUserAccounts.rows,
           message: 'Get all BankAccounts successful!',
         });
-      } else {
-        res.status(404).json({
-          status: 404,
-          error: 'There is no Any Account registered',
-        });
       }
+      res.status(404).json({
+        status: 404,
+        error: 'There is no Any Account registered',
+      });
     } catch (error) {
       console.log(error);
     }
@@ -42,23 +44,17 @@ class UsersController {
 
   static async signup(req, res) {
     const values = [
-      req.body.firstName, req.body.lastName, req.body.userName,
-      bcrypt.hashSync(req.body.password), req.body.phone,
-      req.body.email, 'client', false, req.body.location,
-    ];
-
+      req.body.firstName, req.body.lastName, req.body.userName, bcrypt.hashSync(req.body.password), req.body.phone, req.body.email, 'client', false, req.body.location];
     const inserData = `INSERT INTO
             users("firstName", "lastName", "userName", password, phone, email, type, "isAdmin", location)
             VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
             returning id, "firstName", "lastName", "userName", phone, email, type, "isAdmin", location, "createdDate"`;
     try {
       let checkUser = '';
-
       if (req.body.email) {
         checkUser = await db.query('SELECT * FROM users WHERE "userName"=$1 OR email=$2',
           [req.body.userName, req.body.email]);
       }
-
       if (checkUser.rows.length > 0) {
         res.status(200).json({
           status: 200,
@@ -67,7 +63,6 @@ class UsersController {
       }
 
       const newUser = await db.query(inserData, values);
-
       if (newUser.rows.length > 0) {
         newUser.rows[0].createdDate = new Date(newUser.rows[0].createdDate).toDateString();
         const token = jwt.sign({
@@ -90,15 +85,12 @@ class UsersController {
   }
 
   static async signin(req, res) {
-    // get sign data from the request body
     try {
-      const { rows } = await db.query('SELECT * FROM users WHERE email=$1',
-        [req.body.email]);
-
+      const { rows } = await db.query('SELECT * FROM users WHERE email=$1', [req.body.email]);
       if (rows.length > 0) {
         for (let i = 0; i < rows.length; i += 1) {
           if (bcrypt.compareSync(req.body.password, rows[i].password)) {
-            const isAdmin = !!rows[i].isAdmin; //rows[i].isAdmin ? true : false
+            const isAdmin = !!rows[i].isAdmin; // rows[i].isAdmin ? true : false
             const token = jwt.sign({
               userId: rows[i].id,
               email: rows[i].email,
@@ -107,7 +99,7 @@ class UsersController {
             }, process.env.SECRET_KEY, {
                 expiresIn: 86400, // expires in 24 hours
               });
-            return res.status(200).json({
+            res.status(200).json({
               status: 200,
               data: {
                 id: rows[i].id,
@@ -124,8 +116,7 @@ class UsersController {
           }
         }
       }
-
-      return res.status(400).json({
+      res.status(400).json({
         status: 400,
         error: 'Sorry, your email or password is incorrect',
       });
@@ -134,47 +125,37 @@ class UsersController {
     }
   }
 
-  static async adminCreateUser(req, res) {
+  static checkAdmin(req, res) {
     if (req.decodedToken.isAdmin === false || req.decodedToken.type === 'client') {
-      return res.status(401).json({
+      res.status(401).json({
         message: 'Not allowed to access this feature, Admin Only',
       });
     }
-    const values = [
-      req.body.firstName, req.body.lastName,
-      req.body.userName, bcrypt.hashSync(req.body.password),
-      req.body.phone, req.body.email,
-      'staff', req.body.isAdmin, req.body.location,
-    ];
+  }
 
-    const inserData = `INSERT INTO
-            users("firstName", "lastName", "userName", password, phone, email, type, "isAdmin", location)
-            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            returning id, "firstName", "lastName", "userName", phone, email, type, "isAdmin", location, "createdDate"`;
+  static async adminCreateUser(req, res) {
+    UsersController.checkAdmin(req, res);
+    const values = [req.body.firstName, req.body.lastName, req.body.userName, bcrypt.hashSync(req.body.password),
+    req.body.phone, req.body.email, 'staff', req.body.isAdmin, req.body.location];
+    const inserData = `INSERT INTO users("firstName", "lastName", "userName", password, phone, email, type, "isAdmin", location) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    returning id, "firstName", "lastName", "userName", phone, email, type, "isAdmin", location, "createdDate"`;
     try {
-      let checkUser = '';
-
+      let checkUserStaff = '';
       if (req.body.email) {
-        checkUser = await db.query('SELECT * FROM users WHERE "userName"=$1 OR email=$2 AND password=$3', [req.body.userName, req.body.email, req.body.password]);
-      } else {
-        checkUser = await db.query('SELECT * FROM users WHERE "userName"=$1 AND password=$2', [req.body.userName, req.body.password]);
+        checkUserStaff = await db.query('SELECT * FROM users WHERE "userName"=$1 OR email=$2 AND phone=$3', [req.body.userName, req.body.email, req.body.phone]);
       }
-
-      if (checkUser.rows.length > 0) {
+      if (checkUserStaff.rows.length > 0) {
         res.status(200).json({
           status: 200,
           error: 'Sorry, this account already exists',
         });
       }
-
-      const newUser = await db.query(inserData, values);
-
-      if (newUser.rows.length > 0) {
-        newUser.rows[0].createdDate = new Date(newUser.rows[0].createdDate).toDateString();
+      const newUserStaff = await db.query(inserData, values);
+      if (newUserStaff.rows.length > 0) {
+        newUserStaff.rows[0].createdDate = new Date(newUserStaff.rows[0].createdDate).toDateString();
         res.status(201).json({
           status: 201,
-          data: newUser.rows[0],
-          // token,
+          data: newUserStaff.rows[0],
         });
       }
     } catch (error) {
