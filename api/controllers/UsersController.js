fca3bb1672c2f31210d319223043d73d5f079d81
Celestin NIYONSABA <30776949+niyoceles@users.noreply.github.com>
2019-08-08 13:34:45 +0200

    Updating authentication for users

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -16,7 +16,7 @@ class UsersController {
   }
 
   static async getAllUserAccounts(req, res) {
-    UsersController.checkUser(req, res);
+//     UsersController.checkUser(req, res); removing authentication ?????????????????????
     try {
       let checkUserAccounts = '';
       checkUserAccounts = await db.query('SELECT * FROM users');
